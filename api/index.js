2bb3272cece3ab3e83e1373e0f940b6bb300893d
黄凯 <920672006@qq.com>
2019-08-03 20:14:02 +0800

    add rember

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
 const http = require('./http.js')
 export const getindexInfo = (params) => http.get('v1/search', params)
 export const addHot = (params) => http.get('v1/hot', params)
+export const updata = (params) => http.get('v1/updata', params)
